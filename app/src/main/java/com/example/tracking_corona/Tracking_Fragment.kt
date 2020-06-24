6886115a0d3e1cc6724572018095a2c026d3aed3
Your Nameolosangolo <nguyenquangsanga3@gmail.com>
2020-06-24 23:29:27 +0700

    fix_tracking_layout

--- app/src/main/java/com/example/tracking_corona/Tracking_Fragment.kt
+++ app/src/main/java/com/example/tracking_corona/Tracking_Fragment.kt
@@ -1,10 +1,10 @@
 package com.example.tracking_corona
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -37,23 +37,5 @@ class Tracking_Fragment : Fragment() {
         return inflater.inflate(R.layout.fragment_tracking, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Tracking_Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Tracking_Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
