0882f64dccfe1c2f585311f2e0fe25854a9ce0e6
Your Nameolosangolo <nguyenquangsanga3@gmail.com>
2020-07-09 20:52:21 +0700

    fix frag_info

--- app/src/main/java/Fragment/info_treatment.kt
+++ app/src/main/java/Fragment/info_treatment.kt
@@ -6,40 +6,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.tracking_corona.R
 import kotlinx.android.synthetic.main.fragment_info.*
 
 
 class info_treatment : Fragment() {
-
-
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        button.setOnClickListener {
-            val callIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:19009095"))
-            startActivity(callIntent)
+        lateinit var button : Button
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_info, container, false)
         }
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
 
-        button2.setOnClickListener {
-            val callIntent = Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:1900.9095"))
-            startActivity(callIntent)
+            button = view?.findViewById<Button>(R.id.button_danh_cho_hs)
+
+            button.setOnClickListener {
+                val popupMenu: PopupMenu = PopupMenu(getContext(),button)
+                popupMenu.menuInflater.inflate(R.menu.drop_down_menu1,popupMenu.menu)
+                popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
+                    when(item.itemId) {
+                        R.id.dropdown_menu1 ->
+                            context?.let{ Toast.makeText(getContext(), "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()}
+                        R.id.dropdown_menu2 ->
+                            context?.let{Toast.makeText(getContext(), "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()}
+                    }
+                    true
+                })
+                popupMenu.show()
+            }
         }
-    }
 }
