11dcac85e2b0f0d1f1523bb901fa3b3eca5a1e2b
Alogbrain <nnan22618@gmail.com>
2020-07-09 23:35:02 +0700

    commit

--- app/src/main/java/com/example/tracking_corona/AffectedCountries.kt
+++ app/src/main/java/com/example/tracking_corona/AffectedCountries.kt
@@ -40,8 +40,7 @@ class AffectedCountries : AppCompatActivity(){
 
         loader.start()
         fetchData();
-        loader.stop()
-        loader.setVisibility(View.GONE)
+
 
         supportActionBar?.setTitle("Các quốc gia ảnh hưởng")
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -84,6 +83,8 @@ class AffectedCountries : AppCompatActivity(){
 
             myCustomAdapter = CountriesAdapter(this@AffectedCountries, countryModelsList)
             listView.setAdapter(myCustomAdapter)
+            loader.stop()
+            loader.setVisibility(View.GONE)
         }
     }
 }
